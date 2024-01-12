4034a8db7f436e358d926b8a0df46f3724dcd45f
Cleonildo Soares Guimaraes Junior <cleu.junior@gmail.com>
2024-01-12 15:49:19 -0300

    Initial commit, setting the configuration

---
+++ account.query/account.query/src/main/java/br/com/cleonildo/QueryApplication.java
@@ -0,0 +1,13 @@
+package br.com.cleonildo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryApplication.class, args);
+	}
+
+}
